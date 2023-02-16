4a17a43cda5adc174f6e80615fe70a668f7f908c
ZhouTianyang <SmilingCat.tyz@gmail.com>
2023-02-16 15:38:58 +0800

    add-[PubSub]-添加pubsub消息订阅

--- react_practice/src/App.tsx
+++ react_practice/src/App.tsx
@@ -1,22 +1,10 @@
 import './App.css'
-import axios from 'axios'
+// import axios from 'axios'
 
 export default function App() {
-  const getStudentData = () => {
-    // 通过代理请求数据
-    axios.get('http://localhost:3000/api1').then(
-      response => {
-        console.log('请求成功', response.data);
-      },
-      error => {
-        console.log('请求失败', error);
-
-      }
-    )
-  }
   return (
     <div >
-      <button onClick={getStudentData}>点我获取学生数据</button>
+      <h1>发布订阅</h1>
     </div>
   )
 }
